051dfd26ead9977358d0ae1da88999a848c41be2
yang qin <woodichin@outlook.com>
2024-03-06 17:43:43 +0800

    feat: 🎸 matrix wip

--- app/matrix/page.tsx
+++ app/matrix/page.tsx
@@ -21,13 +21,22 @@ const Matrix: FC = () => {
     console.log(matrix, '----------');
   }, [matrix])
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
+    <div className="w-screen h-screen flex flex-col justify-start items-center gap-4">
       <div>
-        <textarea name="matrix" id="matrix-input" cols={30} rows={10} onChange={inputHandler}></textarea>
+        <textarea name="matrix" id="matrix-input" cols={100} rows={50} onChange={inputHandler}></textarea>
       </div>
-      <div>
+      <div className="flex flex-col justify-start items-center">
         {
-          matrix.map((row, rowIndex) => (<div key={`row-${rowIndex}`}>{row.map((cell, cellIndex) => <div key={`cell-${cellIndex}`}>{cell}</div>)}</div>))
+          matrix.map((row, rowIndex) => (
+            <div className="flex justify-center items-center" key={`row-${rowIndex}`}>
+              {
+                row.map((cell, cellIndex) =>
+                  cell === 1 ?
+                    <div className="w-8 h-8 border border-blue-600 flex justify-center items-center bg-slate-400" key={`cell-${cellIndex}`}>{cell}</div>
+                    : <div className="w-8 h-8 border border-blue-600 flex justify-center items-center" key={`cell-${cellIndex}`}>{cell}</div>
+                )
+              }</div>
+          ))
         }
       </div>
     </div>
